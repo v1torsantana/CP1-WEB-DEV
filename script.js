11efaee7820c42067f302fedc4d21b29afd2b541
Vitor Sant'ana <vitor.ssantana07@gmail.com>
2023-04-07 13:42:40 -0300

    calculando o imc

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-//2.Como usar o operador % em JavaScript?
+//3.Escreva  um  programa  que  calcula  o  índice  de  massa  corporal  (IMC)  de  uma  pessoa 
+// e  exibe  uma mensagem indicando se ela está abaixo, no peso ideal ou acima do peso.
 
-document.write(20 % 6)
-//Calcula o resto da divisão, ou seja, 20 dividido por dois daria 18 (6*3) + 2, sendo 2 o resto
+let peso = prompt('Qual é o seu peso em quilogramas?')
+let altura = prompt('Qual é a sua altura em centímetros?')
+imc = peso/((altura/100) ** 2)
 
-
+if(imc<18.5){
+    document.write('O seu imc é: ' +imc.toFixed(2) +' e você está abaixo do peso ideal!' )
+}else if(imc>18.5 && imc<24.9){
+    document.write('O seu imc é: ' +imc.toFixed(2) + ' e você está dentro da faixa ideal de peso!')
+}else{
+    document.write('O seu imc é: ' +imc.toFixed(2) + ' e você está acima do peso ideal!')
+}
