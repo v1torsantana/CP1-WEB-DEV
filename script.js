8399cd7fb1bf9bbfba6e627e7a703c316d9c9ebf
Vitor Sant'ana <vitor.ssantana07@gmail.com>
2023-04-07 14:27:42 -0300

    calculo de media

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-//5. Crie um programa que recebaum nome de usuário e uma senha e verifique se eles correspondem aos dados de um usuário cadastrado.
+//6.Vamos criar um programa que calcule a média de 7 notas e exiba uma mensagem indicando se o aluno foi aprovado ou reprovado.
 
+let n1 = parseFloat(prompt('Qual é a primeira nota?'))
+let n2 = parseFloat(prompt('Qual é a segunda nota?'))
+let n3 = parseFloat(prompt('Qual é a terceira nota?'))
+let n4 = parseFloat(prompt('Qual é a quarta nota?'))
+media = (n1 + n2 + n3 + n4) / 4
 
-let user1 = prompt('Qual é o usuário?')
-let senha1 = prompt('Qual é a senha?')
+if (media >= 6){
+    document.write('Parabéns, você foi aprovado com a media ' +media+ ' !')
 
-if(user1 == 'admin' && senha1 == 1234){
-    window.alert('Login efetuado com sucesso! Seja bem-vindo.')
 }else{
-    window.alert('Dados incorretos, revise o usuário e/ou a senha.')
+    document.write('Infelizmente sua nota ' +media+ ' não foi suficiente, você foi reprovado!')
 }
-
